5dac99e7ab3b60bf51e9d69d67a3563dffdb676c
mohsenXAD <mohsenxad@gmail.com>
2022-11-21 17:07:47 +0330

    add fist init no methods

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+require('dotenv').config();
+
+const packageJson = require('./package.json');
+
+var app = express();
+app.use(bodyParser.json())
+
+app.use(function(req, res, next) 
+    {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token, eventid, challengeid");
+        next();
+    }
+);
+
+app.get('/isAlive', 
+    (req, res) => 
+        {
+            const result = {
+                emoji : '🙂',
+                name: packageJson.name,
+                version : packageJson.version
+            }
+            sendResult(res, result);
+        }
+);
+
+app.get('/user/getAll',
+    async (req, res) =>
+        {
+            const result = {
+                userList: []
+            };
+            sendResult(
+                res,
+                result
+            )
+        }
+)
+
+function sendResult
+(
+    res,
+    data
+)
+    {
+        res.json(data);
+    }
+
+function processError(
+    res,
+    error
+)
+    {
+        console.log(error);
+        res.status(400).json(
+            {
+                message: error.message 
+            }
+        );
+    }
+
+app.listen(packageJson.port,function()
+    {
+        console.log('Init ' + packageJson.name + ' on ' + packageJson.port);
+        console.log('Access URL : http://localhost:' + packageJson.port);
+    }
+);
